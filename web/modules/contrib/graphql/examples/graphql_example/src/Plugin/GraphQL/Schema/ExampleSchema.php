0bc45b021c5e7c25361955b26ee1c17f70c64834
Carlos Dinarte <carlos@octahedroid.com>
2021-02-10 12:12:39 -0600

    feat: add TextParagraph resolver

--- web/modules/contrib/graphql/examples/graphql_example/src/Plugin/GraphQL/Schema/ExampleSchema.php
+++ web/modules/contrib/graphql/examples/graphql_example/src/Plugin/GraphQL/Schema/ExampleSchema.php
@@ -7,6 +7,8 @@ use Drupal\graphql\GraphQL\ResolverBuilder;
 use Drupal\graphql\GraphQL\ResolverRegistry;
 use Drupal\graphql\Plugin\GraphQL\Schema\SdlSchemaPluginBase;
 use Drupal\graphql_examples\Wrappers\QueryConnection;
+use GraphQL\Error\Error;
+use Drupal\paragraphs\Entity\Paragraph;
 
 /**
  * @Schema(
@@ -14,15 +16,17 @@ use Drupal\graphql_examples\Wrappers\QueryConnection;
  *   name = "Example schema"
  * )
  */
-class ExampleSchema extends SdlSchemaPluginBase {
+class ExampleSchema extends SdlSchemaPluginBase
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getResolverRegistry() {
+  public function getResolverRegistry()
+  {
     $builder = new ResolverBuilder();
     $registry = new ResolverRegistry();
-    
+
     $this->addQueryFields($registry, $builder);
     $this->addArticleFields($registry, $builder);
     $this->addPagesFields($registry, $builder);
@@ -40,13 +44,18 @@ class ExampleSchema extends SdlSchemaPluginBase {
    */
 
 
-  protected function addArticleFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver('Article', 'id',
+  protected function addArticleFields(ResolverRegistry $registry, ResolverBuilder $builder)
+  {
+    $registry->addFieldResolver(
+      'Article',
+      'id',
       $builder->produce('entity_id')
         ->map('entity', $builder->fromParent())
     );
 
-    $registry->addFieldResolver('Article', 'title',
+    $registry->addFieldResolver(
+      'Article',
+      'title',
       $builder->compose(
         $builder->produce('entity_label')
           ->map('entity', $builder->fromParent()),
@@ -55,7 +64,9 @@ class ExampleSchema extends SdlSchemaPluginBase {
       )
     );
 
-    $registry->addFieldResolver('Article', 'author',
+    $registry->addFieldResolver(
+      'Article',
+      'author',
       $builder->compose(
         $builder->produce('entity_owner')
           ->map('entity', $builder->fromParent()),
@@ -64,23 +75,32 @@ class ExampleSchema extends SdlSchemaPluginBase {
       )
     );
 
-    $registry->addFieldResolver('Article', 'description',
+    $registry->addFieldResolver(
+      'Article',
+      'description',
       $builder->fromPath("entity:node", "field_description.value")
     );
 
-    $registry->addFieldResolver('Article', 'descriptionRichText',
+    $registry->addFieldResolver(
+      'Article',
+      'descriptionRichText',
       $builder->fromPath("entity:node", "field_description_rich_text.value")
     );
   }
 
 
-  protected function addPagesFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver('Page', 'id',
+  protected function addPagesFields(ResolverRegistry $registry, ResolverBuilder $builder)
+  {
+    $registry->addFieldResolver(
+      'Page',
+      'id',
       $builder->produce('entity_id')
         ->map('entity', $builder->fromParent())
     );
 
-    $registry->addFieldResolver('Page', 'title',
+    $registry->addFieldResolver(
+      'Page',
+      'title',
       $builder->compose(
         $builder->produce('entity_label')
           ->map('entity', $builder->fromParent()),
@@ -89,55 +109,80 @@ class ExampleSchema extends SdlSchemaPluginBase {
       )
     );
 
-    $registry->addFieldResolver('Page', 'content',
-    $builder->produce('entity_reference_revisions', [
+    $registry->addFieldResolver(
+      'Page',
+      'content',
+      $builder->produce('entity_reference_revisions', [
         'entity' => $builder->fromParent(),
         'field' => $builder->fromValue('field_content'),
-    ])
-  );
+      ])
+    );
   }
 
-  protected function addTextParagraphFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver('Text', 'id',
+  protected function addTextParagraphFields(ResolverRegistry $registry, ResolverBuilder $builder)
+  {
+    $registry->addFieldResolver(
+      'Text',
+      'id',
       $builder->produce('entity_id')
         ->map('entity', $builder->fromParent())
     );
-    
-    $registry->addFieldResolver('Text', 'text',
+
+    $registry->addFieldResolver(
+      'Text',
+      'text',
       $builder->fromPath("entity:node", "field_text.value")
     );
 
-    $registry->addFieldResolver('Text', 'title',
+    $registry->addFieldResolver(
+      'Text',
+      'title',
       $builder->fromPath("entity:node", "field_title.value")
     );
-  
   }
 
   /**
    * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
    * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
    */
-  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver('Query', 'article',
+  protected function addQueryFields(ResolverRegistry $registry, ResolverBuilder $builder)
+  {
+    $registry->addFieldResolver(
+      'Query',
+      'article',
       $builder->produce('entity_load')
         ->map('type', $builder->fromValue('node'))
         ->map('bundles', $builder->fromValue(['article']))
         ->map('id', $builder->fromArgument('id'))
     );
 
-    $registry->addFieldResolver('Query', 'pages',
+    $registry->addFieldResolver(
+      'Query',
+      'pages',
       $builder->produce('query_base')
         ->map('offset', $builder->fromArgument('offset'))
         ->map('type', $builder->fromValue('page'))
         ->map('limit', $builder->fromArgument('limit'))
     );
 
-    $registry->addFieldResolver('Query', 'articles',
+    $registry->addFieldResolver(
+      'Query',
+      'articles',
       $builder->produce('query_base')
         ->map('offset', $builder->fromArgument('offset'))
         ->map('type', $builder->fromValue('article'))
         ->map('limit', $builder->fromArgument('limit'))
     );
+
+    $registry->addTypeResolver('Paragraph', function ($value) {
+      if ($value instanceof Paragraph) {
+        switch ($value->bundle()) {
+          case 'text':
+            return 'Text';
+        }
+      }
+      throw new Error('Could not resolve type ' . $value->bundle());
+    });
   }
 
   /**
@@ -145,18 +190,22 @@ class ExampleSchema extends SdlSchemaPluginBase {
    * @param \Drupal\graphql\GraphQL\ResolverRegistry $registry
    * @param \Drupal\graphql\GraphQL\ResolverBuilder $builder
    */
-  protected function addConnectionFields($type, ResolverRegistry $registry, ResolverBuilder $builder) {
-    $registry->addFieldResolver($type, 'total',
+  protected function addConnectionFields($type, ResolverRegistry $registry, ResolverBuilder $builder)
+  {
+    $registry->addFieldResolver(
+      $type,
+      'total',
       $builder->callback(function (QueryConnection $connection) {
         return $connection->total();
       })
     );
 
-    $registry->addFieldResolver($type, 'items',
+    $registry->addFieldResolver(
+      $type,
+      'items',
       $builder->callback(function (QueryConnection $connection) {
         return $connection->items();
       })
     );
   }
-
 }
